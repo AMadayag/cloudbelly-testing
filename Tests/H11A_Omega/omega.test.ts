5c10d4bc79835a33cd5299daf0678c29b307600a
AMadayag <hiding303@gmail.com>
2026-04-18 14:54:51 +1000

    idk

--- Tests/H11A_Omega/omega.test.ts
+++ Tests/H11A_Omega/omega.test.ts
@@ -1,52 +1,53 @@
 import { getDataCoords, getDataWeatherStation } from "./omega.testhelper";
 
-// test for createInvoice
-describe('createInvoice', () => {
-  let token: string;
-  beforeAll(async () => {
-    const login = await loginUserReq(
-      'user@gmail.com',
-      'passworD123!'
-    );
-    token = login.data.data.token;
-  })
+// three day range so that it's not too much data
+const dateStart = '2026-01-02'
+const dateEnd = '2026-01-05'
 
-  test('successful creation', async () => {
-    const res = await createInvoiceReq(MOCK_INVOICE, token);
+// test for getData
+describe('getData', () => {
+  test('successful getData, weather station, temperature', async () => {
+    const res = await getDataCoords(
+      'sydney_(observatory_hill)',
+      'NSW',
+      'temperature',
+      dateStart,
+      dateEnd
+    );
     expect(res.status).toBe(200);
     expect(res.data.invoice).toStrictEqual(expect.any(String));
   });
 
-  test('missing fields', async () => {
-    try {
-      await createInvoiceReq(MISSING_FIELDS, token);
-      fail('Did not throw expected error');
-    } catch (error) {
-      if (error instanceof Error) {
-        const axiosError = error as any;
-        expect(axiosError.response.status).toBe(400);
-        expect(axiosError.response.data.status).toStrictEqual(expect.any(String));
-        expect(axiosError.response.data.error).toStrictEqual(expect.any(String));
-      } else {
-        throw error;
-      }
-    }
-  });
+  // test('missing fields', async () => {
+  //   try {
+  //     await createInvoiceReq(MISSING_FIELDS, token);
+  //     fail('Did not throw expected error');
+  //   } catch (error) {
+  //     if (error instanceof Error) {
+  //       const axiosError = error as any;
+  //       expect(axiosError.response.status).toBe(400);
+  //       expect(axiosError.response.data.status).toStrictEqual(expect.any(String));
+  //       expect(axiosError.response.data.error).toStrictEqual(expect.any(String));
+  //     } else {
+  //       throw error;
+  //     }
+  //   }
+  // });
 
-  test('invalid token', async () => {
-    try {
-      await createInvoiceReq(MOCK_INVOICE, 'invalidToken');
-      fail('Did not throw expected error');
-    } catch (error) {
-      if (error instanceof Error) {
-        const axiosError = error as any;
-        expect(axiosError.response.status).toBe(401);
-        expect(axiosError.response.data.status).toStrictEqual(expect.any(String));
-        expect(axiosError.response.data.message).toStrictEqual(expect.any(String));
-      } else {
-        throw error;
-      }
-    }
-  });
+  // test('invalid token', async () => {
+  //   try {
+  //     await createInvoiceReq(MOCK_INVOICE, 'invalidToken');
+  //     fail('Did not throw expected error');
+  //   } catch (error) {
+  //     if (error instanceof Error) {
+  //       const axiosError = error as any;
+  //       expect(axiosError.response.status).toBe(401);
+  //       expect(axiosError.response.data.status).toStrictEqual(expect.any(String));
+  //       expect(axiosError.response.data.message).toStrictEqual(expect.any(String));
+  //     } else {
+  //       throw error;
+  //     }
+  //   }
+  // });
   
 });
